ddf7a752efd1c40d1bc672fecfed092ba8988f2e
Josh Cox <josh@webhosting.coop>
2015-06-29 03:03:54 -0500

    ports

---
+++ ports.sh
@@ -0,0 +1,13 @@
+#!/binbash
+ufw allow 2302/udp
+ufw allow 2303/udp
+ufw allow 2304/udp
+ufw allow 2305/udp
+ufw allow 2344/udp
+ufw allow 2344/tcp
+ufw allow 2345/udp
+ufw allow 2345/tcp
+ufw allow 27005/udp
+ufw allow 27015/udp
+ufw allow 27020/udp
+ufw allow 44180/tcp
