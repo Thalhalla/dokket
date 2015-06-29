1bbf8ba7878ea8f6ebd6d51206cad0ef5b7522b0
Josh Cox <josh@webhosting.coop>
2015-06-28 20:17:27 -0500

    run

--- run.sh
+++ run.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-cd /home/steam
-./tf2server start
-echo "started server"
-sleep 10
-echo "retreiving details"
-./tf2server monitor
-sleep 3
-sleep 300
+echo "starting server"
+service php5-fpm start && nginx
+
 # infinite loop to keep it open for Docker
-while true; ./tf2server monitor; sleep 300; done
+while true; echo monitor; sleep 300; done
